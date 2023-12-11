01c4ff68c3e80c5d7271bde9878830351abe6917
Rashad133 <tu82907hw@code.edu.az>
2023-12-12 00:43:12 +0400

    Add Searching and Sorting

--- ProniaBackEnd/Controllers/ShopController.cs
+++ ProniaBackEnd/Controllers/ShopController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaBackEnd.DAL;
+using ProniaBackEnd.Models;
+using ProniaBackEnd.ViewModels;
 
 namespace ProniaBackEnd.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        public ShopController(AppDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index(string search,int? order=1)
+        {
+            IQueryable<Product> query = _db.Products.Include(p => p.ProductImages).AsQueryable();
+
+            switch (order)
+            {
+                case 1:
+                    query = query.OrderBy(p => p.Name);
+                    break;
+                case 2:
+                    query = query.OrderBy(p => p.Price);
+                    break;
+                case 3:
+                    query = query.OrderByDescending(p => p.Id);
+                    break;
+            }
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                query = query.Where(p=>p.Name.ToLower().Contains(search.ToLower()));
+            }
+            ShopVM shopVM = new ShopVM
+            {
+                Categories = await _db.Categories.Include(c=>c.Products).ToListAsync(),
+                Products = await _db.Products.Include(p=>p.ProductImages).ToListAsync()
+            };
+            return View(shopVM);
         }
     }
 }
---
+++ ProniaBackEnd/ViewModels/ShopVM.cs
@@ -0,0 +1,11 @@
+﻿using ProniaBackEnd.Models;
+
+namespace ProniaBackEnd.ViewModels
+{
+    public class ShopVM
+    {
+
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
